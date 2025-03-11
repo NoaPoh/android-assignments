f0876a5e5a8ac75f1807057b5d0500a2500df76a
נועה <noa@>
2025-03-11 17:45:10 +0200

    add toastr when review is created successfully

--- brook/app/src/main/java/com/example/brook/modules/createReview/CreateReview.kt
+++ brook/app/src/main/java/com/example/brook/modules/createReview/CreateReview.kt
@@ -131,6 +131,10 @@ class CreateReview : Fragment() {
                 viewModel.createReview(choosedBook) {
                     findNavController().navigate(R.id.action_create_review_to_main_feed)
                     binding.saveButton.isClickable = true
+                    Toast.makeText(
+                        requireContext(),
+                        "Review created successfully",
+                        Toast.LENGTH_SHORT).show()
                 }
             } else {
                 // Handle the case when chooseBook argument is missing
